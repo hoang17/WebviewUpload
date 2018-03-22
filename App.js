b64abe4e3a29c5d1874a3014e93a4fec67c7c684
Hoang Le <lehuyhoang117@gmail.com>
2018-03-23 02:01:09 +0700

    init

--- App.js
+++ App.js
@@ -2,22 +2,7 @@ import React, { Component } from 'react'
 import { WebView, Image, Linking, Button } from 'react-native'
 import ImagePicker from 'react-native-image-picker'
 
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native'
-
-// // Launch Camera:
-// ImagePicker.launchCamera(options, (response)  => {
-//   // Same code as in above section!
-// })
-
-// Open Image Library:
-// ImagePicker.launchImageLibrary(options, (response)  => {
-//   // Same code as in above section!
-// })
+import { StyleSheet, View } from 'react-native'
 
 var options = {
   title: 'Select Photo',
@@ -26,34 +11,6 @@ var options = {
     path: 'images'
   }
 }
-
-/**
- * The first arg is the options object for customization (it can also be null or omitted for default options),
- * The second arg is the callback which sends object: response (more info below in README)
- */
-// ImagePicker.showImagePicker(options, (response) => {
-//   console.log('Response = ', response)
-//
-//   if (response.didCancel) {
-//     console.log('User cancelled image picker')
-//   }
-//   else if (response.error) {
-//     console.log('ImagePicker Error: ', response.error)
-//   }
-//   else if (response.customButton) {
-//     console.log('User tapped custom button: ', response.customButton)
-//   }
-//   else {
-//     let source = { uri: response.uri }
-//
-//     // You can also display the image using data:
-//     // let source = { uri: 'data:image/jpegbase64,' + response.data }
-//
-//     this.setState({
-//       avatarSource: source
-//     })
-//   }
-// })
 
 const baseUrl = "http://192.168.100.11:3000/"
 const uploadUrl = baseUrl + 'upload'
@@ -107,7 +64,7 @@ export default class App extends Component<Props> {
     const { uri } = this.state
     return (
       <View style={styles.container}>
-        {/* <Image source={this.state.avatarSource} style={styles.uploadAvatar} /> */}
+        {/* <Image source={this.state.avatarSource} style={styles.uploadPhoto} /> */}
         <WebView
           source={{ uri }}
           style={styles.webview}
@@ -131,12 +88,7 @@ const styles = StyleSheet.create({
   webview: {
     flex: 1
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  uploadAvatar: {
+  uploadPhoto: {
     width: 300,
     height: 300,
   }
